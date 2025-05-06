58f9ce5ebc546643400231da110b1be557028796
GuilhermePFA <guilherme.pfa@puccampinas.edu.br>
2025-05-05 22:04:13 -0300

    Revert "foi adicionado o modal na tela de risk"
    This reverts commit d5ad9fa1928fb6e5c2044663a4b0ec927c4e046f.

--- app/src/main/java/com/example/pi_03_equipe_01/MainActivity.kt
+++ app/src/main/java/com/example/pi_03_equipe_01/MainActivity.kt
@@ -41,9 +41,9 @@ class MainActivity : AppCompatActivity() {
                     val intent = Intent(this, History::class.java)
                     startActivity(intent)
                 }
-                R.id.nav_risk -> {
+                R.id.nav_signup -> {
                     // Ação para abrir a tela "SignUp"
-                    val intent = Intent(this, Risk::class.java)
+                    val intent = Intent(this, SignUp::class.java)
                     startActivity(intent)
                 }
                 R.id.nav_sair -> {
--- app/src/main/java/com/example/pi_03_equipe_01/Risk.kt
+++ app/src/main/java/com/example/pi_03_equipe_01/Risk.kt
@@ -14,13 +14,10 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import androidx.core.view.GravityCompat
-import androidx.drawerlayout.widget.DrawerLayout
 import com.example.pi_03_equipe_01.databinding.ActivityRiskBinding
 import com.google.android.gms.common.api.Api.Client
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
-import com.google.android.material.navigation.NavigationView
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
@@ -31,7 +28,6 @@ import kotlin.random.Random
 
 
 class Risk : AppCompatActivity() {
-    private lateinit var drawerLayout: DrawerLayout
     private val auth = FirebaseAuth.getInstance()
     private lateinit var binding: ActivityRiskBinding
     lateinit var fusedLocationProviderClient:FusedLocationProviderClient
@@ -114,34 +110,6 @@ class Risk : AppCompatActivity() {
                         .setBackgroundTint(Color.RED)
                         .show()
                 }
-        }
-        drawerLayout = findViewById(R.id.nav_risk)
-
-        val navigationView: NavigationView = findViewById(R.id.nav_view)
-
-        val menuButton: TextView = findViewById(R.id.menu_button)
-        menuButton.setOnClickListener {
-            drawerLayout.openDrawer(GravityCompat.START)
-        }
-
-        navigationView.setNavigationItemSelectedListener { menuItem ->
-            when (menuItem.itemId) {
-                R.id.nav_home -> {
-                    startActivity(Intent(this, MainActivity::class.java))
-                }
-                R.id.nav_history -> {
-                    startActivity(Intent(this, History::class.java))
-                    finish()
-                }
-                R.id.nav_risk -> {
-                    startActivity(Intent(this, Risk::class.java))
-                }
-                R.id.nav_sair -> {
-                    finishAffinity()
-                }
-            }
-            drawerLayout.closeDrawer(GravityCompat.START)
-            true
         }
     }
 
